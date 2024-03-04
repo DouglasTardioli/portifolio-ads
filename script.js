879014d5043c86f6394dec1caefd491ee0511d39
Douglas Tardioli <douglastardioli48@gmail.com>
2024-03-04 18:23:14 -0300

    novas telas, js, css

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
-// // Seleciona o botão e a seção que você quer ir
-// const botao = document.querySelector('#botao');
-// const secao = document.querySelector('#secao');
 
-// // Adiciona um evento de clique no botão
-// botao.addEventListener('click', function() {
-//   // Usa o método scrollIntoView para ir até a seção com uma transição suave
-//   secao.scrollIntoView({ behavior: 'smooth' });
-// });
+const sobre = document.querySelector('#sobre-mim');
+const portifolio = document.querySelector('#portifolio');
+const formacao = document.querySelector('#formacao');
+const contato = document.querySelector('#contato');
+const secaoSobre = document.querySelector('#secao-sobre');
+const secaoFormacao = document.querySelector('#secao-formacao');
+const secaoContato = document.querySelector('#secao-contato');
+const secaoPortifolio = document.querySelector('#secao-portifolio');
+
+
+sobre.addEventListener('click', () => {
+  secaoSobre.scrollIntoView({ behavior: 'smooth' });
+});
+formacao.addEventListener('click', () => {
+  secaoFormacao.scrollIntoView({ behavior: 'smooth' });
+});
+contato.addEventListener('click', () => {
+  secaoContato.scrollIntoView({ behavior: 'smooth' });
+});
+portifolio.addEventListener('click', () => {
+  secaoPortifolio.scrollIntoView({ behavior: 'smooth' });
+});
 
 
 const icon = document.getElementById("icon-menu");
